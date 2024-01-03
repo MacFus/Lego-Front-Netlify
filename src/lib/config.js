f4258a807082f65b99155930bfcdca7793a91a3b
mfus <fus.maciek@gmail.com>
2024-01-03 19:23:48 +0100

    new backend v1

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,2 +1,3 @@
 // export const BASE_URL = 'http://localhost:8080/api/v1';
-export const BASE_URL = 'https://lego.agreeableplant-5f34a27d.westeurope.azurecontainerapps.io/api/v1';
+// export const BASE_URL = 'https://lego.agreeableplant-5f34a27d.westeurope.azurecontainerapps.io/api/v1';
+export const BASE_URL = 'https://lego-v1.agreeableplant-5f34a27d.westeurope.azurecontainerapps.io/api/v1';
